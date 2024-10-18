85b47983df621c175044f2818040340d1e59454c
FleetCentere <mcgrathan@gmail.com>
2024-10-18 11:36:16 -0400

    add config.py and secret key

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,21 @@ from flask import render_template
 from app import app
 
 user = {"username": "Andrew"}
+posts = [
+    {
+        "author": {"username": "Steve"},
+        "body": "This is a post from Steve"
+    },
+    {
+        "author": {"username": "Hampus"},
+        "body": "I am a defenseman!"
+    }
+]
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", user=user)
+    return render_template("index.html", user=user, posts=posts)
 
 @app.route("/about")
 def about():
