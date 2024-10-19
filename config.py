9bbfc002f5ad2c26a1b5893f698c6b96a56186fa
FleetCentere <mcgrathan@gmail.com>
2024-10-18 21:58:12 -0400

    added postgres database and models.py

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "I-Hope-This-Is-Secure"
-
     
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql+psycopg2://postgres:postgres@localhost/spheres"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
