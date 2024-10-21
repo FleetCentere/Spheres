c89f7a627b10e7d1617f78886bc6a50407d29046
FleetCentere <mcgrathan@gmail.com>
2024-10-20 21:47:18 -0400

    finish chapter 6 on profile page

--- app/routes.py
+++ app/routes.py
@@ -2,27 +2,22 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 import sqlalchemy as sa
+from datetime import datetime, timezone
 
-user = {"username": "Andrew"}
-posts = [
-    {
-        "author": {"username": "Steve"},
-        "body": "This is a post from Steve"
-    },
-    {
-        "author": {"username": "Hampus"},
-        "body": "I am a defenseman!"
-    }
-]
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
 
 @app.route("/")
 @app.route("/index")
 @login_required
 def index():
-    return render_template("index.html", posts=posts)
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -69,6 +64,21 @@ def user(username):
         {"author": user, "body": "This is yet another test post"}
     ]
     return render_template("user.html", user=user, posts=posts)
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", form=form)
 
 @app.route("/about")
 def about():
